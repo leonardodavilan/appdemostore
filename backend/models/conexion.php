4115b335eab1a749c5fcbf5b468dc84312f97bd9
Leonardo Davila <davilanapa@gmail.com>
2020-07-24 11:56:10 -0500

    conexionmodificada

--- backend/models/conexion.php
+++ backend/models/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=tienda_demo","root","");
+		$link = new PDO("mysql:host=qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=h4mm55t2l7sh5f9v","t93vsxhvr61swckp","	cdhjx6y1heo107gx");
 		return $link;
 
 	}
