dd7a9a39674cc6e1ec3e70acce08ce3a5bfa110c
Leonardo Davila <davilanapa@gmail.com>
2020-07-24 12:28:52 -0500

    newmodifi

--- backend/models/conexion.php
+++ backend/models/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=h4mm55t2l7sh5f9v","t93vsxhvr61swckp","	cdhjx6y1heo107gx");
+		$link = new PDO("mysql:host=qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=h4mm55t2l7sh5f9v","t93vsxhvr61swckp","cdhjx6y1heo107gx");
 		return $link;
 
 	}
